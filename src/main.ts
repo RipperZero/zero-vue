a0810cdf6cbea800780c57c7842db4e27b9d45e1
ZiWen-Liu <ziwen.liu@sayweee.com>
2022-07-25 17:55:31 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount('#app')
+// createApp({
+//   data() {
+//     return {
+//       author: {
+//         name: "John Doe",
+//         books: [
+//           "Vue 2 - Advanced Guide",
+//           "Vue 3 - Basic Guide",
+//           "Vue 4 - The Mystery",
+//         ],
+//       },
+//     };
+//   },
+// }).mount("#test");
